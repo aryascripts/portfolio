a2fc7141f000c189d27626d6633f33318228205c
Aman Bhimani <amanb014@gmail.com>
2019-03-03 13:51:31 -0500

    updated cards

--- components/Card.vue
+++ components/Card.vue
@@ -1,11 +1,13 @@
 <template>
   <div
     v-if="project"
-    v-bind:style="{
-      'border-left': num % 2 === 1 ? '12px solid white' : 'none',
-      'border-right': num % 2 === 0 ? '12px solid white' : 'none'
+    v-bind:class="{
+      'left-card': num % 2 === 0,
+      'right-card': num % 2 === 1
       }"
     class="card">
+
+    <h2>{{ project.name }}</h2>
 
   </div>
 
@@ -65,20 +67,33 @@ export default {
     box-sizing: unset;
     overflow: hidden;
     background-position: top center;
-    cursor: pointer;
     -webkit-transition: all 200ms ease-in;
     transition: all 200ms ease-in;
+    padding: 0 20px;
 
     @include transition(all 200ms ease-in);
 
     &:hover {
       background-color: rgba(255, 255, 255, 0.07);
     }
+
+    display: flex;
+    flex-direction: column;
   }
 
   %font {
     font-family: 'Helvetica', 'Verdana', sans-serif;
     font-weight: 300;
+  }
+
+  .left-card {
+    border-left: 12px solid white;
+    align-items: flex-start;
+  }
+
+  .right-card {
+    border-right: 12px solid white;
+    align-items: flex-end;
   }
 
 
