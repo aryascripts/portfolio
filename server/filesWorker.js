4652f45f106130fde2f6d519db3f06a882cf2d39
Aman Bhimani <amanb014@gmail.com>
2019-03-16 16:38:03 -0400

    adde filesWorker for projects list (storing in cache during server initialization)

--- server/filesWorker.js
+++ server/filesWorker.js
@@ -1,17 +1,41 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
+const md = require('markdown-it')()
+const YAML = require('yamljs')
 
 module.exports = class FilesWorker {
 
   constructor(path) {
     this.path = path;
+    this.storeInCache();
   }
 
   getFolderFilesList() {
     return new Promise((resolve, reject) => {
       fs.readdir(path.join(__dirname, this.path), (err, files) => {
-        resolve(files);
+        resolve(files)
       })
-    });
+    })
+  }
+
+  async storeInCache() {
+    this.files = [];
+    const files = await this.getFolderFilesList()
+    for(let i = 0; i < files.length; i++) {
+      const p = path.join(__dirname, this.path, files[i])
+      const content = fs.readFileSync(p, 'utf8')
+      this.files.push(this.getJSONFromFile(content))
+    }
+    return this.files;
+  }
+
+  getFiles() {
+    if(this.files.length) return Promise.resolve(this.files);
+
+    return this.storeInCache();
+  }
+
+  getJSONFromFile(fileContent) {
+    return YAML.parse(fileContent.split('---')[1]);
   }
 }
--- server/projectRoutes.js
+++ server/projectRoutes.js
@@ -1,16 +1,14 @@
 const { Router } = require('express')
 
 const router = Router()
-const fs = require('fs')
 const path = require('path')
 const FilesWorker = require('./filesWorker')
 
-const projectWorker = new FilesWorker('../content/projects');
-
+const projectWorker = new FilesWorker('../content/projects')
 
 router.get('/list', async (req, res) => {
-  const files = await projectWorker.getFolderFilesList();
-  res.status(200).send(files);
+  const files = await projectWorker.getFiles()
+  res.status(200).send(files)
 })
 
 module.exports = router
